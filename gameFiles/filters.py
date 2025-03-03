4309738ba35615fa2ec7f01269abfb7066e172d1
Steffen <steffen@noxt.de>
2025-03-03 20:55:39 +0100

    feat: refactor filters to use a base class for dynamic filter generation

--- gameFiles/filters.py
+++ gameFiles/filters.py
@@ -1,56 +1,49 @@
 import django_filters
 from dal import autocomplete
-
 from .models import Tag, Image, Sound, Question, Category, Hints, WhoKnowsMore
 
 
-class ProfileFilter(django_filters.FilterSet):
+class BaseFilter(django_filters.FilterSet):
+    """Mixin to define common filter fields for multiple models."""
     solution = django_filters.CharFilter(lookup_expr='icontains', label="Lösung")
-    category = django_filters.ModelMultipleChoiceFilter(queryset=Category.objects.all(),
-                                                        widget=autocomplete.ModelSelect2Multiple(
-                                                            url='gamefiles:category-autocomplete'), label="Kategorie")
-    tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(),
-                                                    widget=autocomplete.ModelSelect2Multiple(
-                                                        url='gamefiles:tag-autocomplete'))
+    category = django_filters.ModelMultipleChoiceFilter(
+        queryset=Category.objects.all(),
+        widget=autocomplete.ModelSelect2Multiple(url='gamefiles:category-autocomplete'),
+        label="Kategorie"
+    )
+    tags = django_filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
+        widget=autocomplete.ModelSelect2Multiple(url='gamefiles:tag-autocomplete')
+    )
     private_new = django_filters.BooleanFilter(label='Privat')
 
-
-class ImageFilter(ProfileFilter):
     class Meta:
-        model = Image
-        fields = ['solution', 'category', 'private_new', 'tags']
-        order_by = ['pk']
+        abstract = True
 
 
-class SoundFilter(ProfileFilter):
-    class Meta:
-        model = Sound
-        fields = ['solution', 'category', 'private_new', 'tags']
-        order_by = ['pk']
+class BaseProfileFilter(BaseFilter):
+    """Base filter for Image, Sound, Question, Hints, and WhoKnowsMore models."""
+    
+    @classmethod
+    def create_filter(cls, model):
+        """Dynamically creates a filter class for the given model."""
+        return type(
+            f"{model.__name__}Filter",
+            (cls,),
+            {"Meta": type("Meta", (), {"model": model, "fields": ['solution', 'category', 'private_new', 'tags'], "order_by": ['pk']})}
+        )
 
 
-class QuestionFilter(ProfileFilter):
-    class Meta:
-        model = Question
-        fields = ['solution', 'category', 'private_new', 'tags']
-        order_by = ['pk']
-
-
-class HintFilter(ProfileFilter):
-    class Meta:
-        model = Hints
-        fields = ['solution', 'category', 'private_new', 'tags']
-        order_by = ['pk']
-
-
-class WhoKnowsMoreFilter(ProfileFilter):
-    class Meta:
-        model = WhoKnowsMore
-        fields = ['solution', 'category', 'private_new', 'tags']
-        order_by = ['pk']
+# Generate filter classes dynamically
+ImageFilter = BaseProfileFilter.create_filter(Image)
+SoundFilter = BaseProfileFilter.create_filter(Sound)
+QuestionFilter = BaseProfileFilter.create_filter(Question)
+HintFilter = BaseProfileFilter.create_filter(Hints)
+WhoKnowsMoreFilter = BaseProfileFilter.create_filter(WhoKnowsMore)
 
 
 class CategoryFilter(django_filters.FilterSet):
+    """Filter for Category model."""
     class Meta:
         model = Category
         fields = ['private']
