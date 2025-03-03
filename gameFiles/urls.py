c3a30604828571dd92f42b3c2fe7b737be54a97e
Steffen <steffen@noxt.de>
2025-03-03 21:00:27 +0100

    feat: streamline URL patterns with dynamic CRUD operations for multiple models

--- gameFiles/urls.py
+++ gameFiles/urls.py
@@ -1,39 +1,36 @@
 from django.urls import path
-
 from . import views
 from account import views as account_views
 
 urlpatterns = [
+    # General Views
     path('', views.GameTypeView.as_view(), name='game_type_list'),
     path('categories/<int:game_type>/', views.CategoryView.as_view(), name='category_list'),
     path('<int:game_type>/<int:id>/', views.category_detail, name='category_detail'),
-    path('category-create', views.CategoryCreateView.as_view(), name='category-create'),
-    path('sound-create', views.SoundCreateView.as_view(), name='sound-create'),
-    path('image-create', views.ImageCreateView.as_view(), name='image-create'),
-    path('question-create', views.QuestionCreateView.as_view(), name='question-create'),
-    path('hint-create', views.HintCreateView.as_view(), name='hint-create'),
-    path('whoknowsmore-create', views.WhoKnowsMoreCreateView.as_view(), name='whoknowsmore-create'),
     path('solution/<int:game_type>/<int:category_element>/', views.solution, name='solution'),
+
+    # Category Views
+    path('category-create', views.CategoryCreateView.as_view(), name='category-create'),
     path('category-edit/<int:pk>', views.CategoryEditView.as_view(), name='category-edit'),
-    path('image-edit/<int:pk>', views.ImageEditView.as_view(), name='image-edit'),
-    path('sound-edit/<int:pk>', views.SoundEditView.as_view(), name='sound-edit'),
-    path('question-edit/<int:pk>', views.QuestionEditView.as_view(), name='question-edit'),
-    path('hint-edit/<int:pk>', views.HintEditView.as_view(), name='hint-edit'),
-    path('whoknowsmore-edit/<int:pk>', views.WhoKnowsMoreEditView.as_view(), name='whoknowsmore-edit'),
     path('category-delete/<int:pk>', views.CategoryDeleteView.as_view(), name='category-delete'),
-    path('image-delete/<int:pk>', views.ImageDeleteView.as_view(), name='image-delete'),
-    path('sound-delete/<int:pk>', views.SoundDeleteView.as_view(), name='sound-delete'),
-    path('question-delete/<int:pk>', views.QuestionDeleteView.as_view(), name='question-delete'),
-    path('hint-delete/<int:pk>', views.HintDeleteView.as_view(), name='hint-delete'),
-    path('whoknowsmore-delete/<int:pk>', views.WhoKnowsMoreDeleteView.as_view(), name='whoknowsmore-delete'),
-    path('image-download/<int:category_id>', views.ImageDownloadView.as_view(), name='image-download'),
-    path('sound-download/<int:category_id>', views.SoundDownloadView.as_view(), name='sound-download'),
-    path('question-download/<int:category_id>', views.QuestionDownloadView.as_view(), name='question-download'),
-    path('hint-download/<int:category_id>', views.HintDownloadView.as_view(), name='hint-download'),
-    path('whoknowsmore-download/<int:category_id>', views.WhoKnowsMoreDownloadView.as_view(), name='whoknowsmore-download'),
-    path('ajax/get_profile_table/<str:per_page>/', account_views.get_profile_table, name='get_profile_table'),
-    path('ajax/set_profile_filter/<str:per_page>/', account_views.set_profile_filter, name='set_profile_filter'),
+
+    # Autocomplete Views
     path('tag-autocomplete', views.TagAutocomplete.as_view(create_field='name_de'), name='tag-autocomplete'),
     path('category-autocomplete', views.CategoryAutocomplete.as_view(), name='category-autocomplete'),
     path('user-autocomplete', views.UserAutocomplete.as_view(), name='user-autocomplete'),
+
+    # AJAX Calls
+    path('ajax/get_profile_table/<str:per_page>/', account_views.get_profile_table, name='get_profile_table'),
+    path('ajax/set_profile_filter/<str:per_page>/', account_views.set_profile_filter, name='set_profile_filter'),
 ]
+
+# Dynamic CRUD operations for multiple models
+models = ["sound", "image", "question", "hint", "whoknowsmore"]
+
+for model in models:
+    urlpatterns.extend([
+        path(f'{model}-create', getattr(views, f"{model.capitalize()}CreateView").as_view(), name=f'{model}-create'),
+        path(f'{model}-edit/<int:pk>', getattr(views, f"{model.capitalize()}EditView").as_view(), name=f'{model}-edit'),
+        path(f'{model}-delete/<int:pk>', getattr(views, f"{model.capitalize()}DeleteView").as_view(), name=f'{model}-delete'),
+        path(f'{model}-download/<int:category_id>', getattr(views, f"{model.capitalize()}DownloadView").as_view(), name=f'{model}-download'),
+    ])
