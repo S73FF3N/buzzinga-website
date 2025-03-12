b78cdea2904295e235dc337f49520c9b42b940cc
Steffen <steffen@noxt.de>
2025-03-12 21:53:47 +0100

    fix: update delete_column template for correct URL routing based on game type and adjust delete view to handle GET requests

--- gameFiles/views.py
+++ gameFiles/views.py
@@ -213,6 +213,9 @@ class WhoknowsmoreEditView(LoginRequiredMixin, SuccessUrlMixin, UpdateView):
 
 
 class BaseDeleteView(LoginRequiredMixin, SuccessUrlMixin, DeleteView):
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+    
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, f"{self.model.__name__} successfully deleted.")
         return super().delete(request, *args, **kwargs)
