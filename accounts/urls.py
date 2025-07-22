36a2c6aa5511c846db6cfbdbf5a8775da98b0aa3
Steffen <steffen@noxt.de>
2025-07-22 22:15:57 +0200

    renamed app useraccount to accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,6 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
-app_name = 'useraccount'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
