b15cac4bcd8befb2be96efc63f73d19367695170
Steffen <steffen@noxt.de>
2025-07-22 21:41:14 +0200

    updated urls

--- useraccount/urls.py
+++ useraccount/urls.py
@@ -4,12 +4,13 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path("", include("django.contrib.auth.urls")),
+    #path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    #path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    #path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    #path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('profile/<str:per_page>/', views.profile_view, name='profile'),
     path('download/<str:active_table>/<str:element_string>/', views.DownloadView.as_view(), name='download'),
     path('download/<str:active_table>/all/', views.DownloadView.as_view(), name='download_all'),
