73c4c64442c2baf1701ffdb3a9b20fe7421d413e
Steffen Schwarzwald <steffen@noxt.de>
2026-06-29 08:48:56 +0200

    Mealplanner in eigenes Repo ausgelagert

--- mealplanner/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MealplannerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mealplanner'
--- mealplanner/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('webhook/', views.telegram_webhook, name='webhook'),
-]
